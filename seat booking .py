b48e39ffd93db0ca186a9248c88cd87bcfb53881
Priyanka1524-world <priyankabagewadi85@gmail.com>
2025-09-16 20:07:25 +0530

    Added new python program

---
+++ seat booking .py
@@ -0,0 +1,9 @@
+# interactive bus seat booking using while loop
+
+total_seats=int(input("Enter total number of seats in the bus:"))
+seat=1
+
+while seat<=total_seats:
+    print("seat",seat,"booked")
+    seat+=1
+print("all seats are booked!")
